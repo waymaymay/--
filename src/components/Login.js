5ef2670ea8d58924167ca2b296e8c1ffc37dc71a
waymaymay <Rdvickywei@gmail.com>
2023-07-27 22:21:35 +0800

    建立登入頁的離開按鈕

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import Button from './LoginButton/Button';
+import Leave from './leaveButton/leave';
+// import imgs from './imgs/login-image.png';
 
 const Login = () => {
   const handleLogin = () => {
@@ -12,21 +14,20 @@ const Login = () => {
   return (
     <div className="login-container">
         <div className="left-section">
-            <h2>請假登入</h2>
+            <div className='logintop'>請假登入</div>
             {/* Add login form here */}
-            <Button label="登入" onClick={handleLogin} />
+            <div className='login-group'>
+                <Button label="登入" onClick={handleLogin} />
+                <Leave label="離開" onClick={handleLogin} />
+            </div>
+            
         </div>
         
         <div className="right-section">
              {/* Content for the right section */}
-            <img src="https://assets.blog.engoo.com/wp-content/uploads/sites/3/2022/09/07094921/dayoff-1024x597.png.webp"
-            width="600vmin" height="400vmin" alt='picture'/>
+            <img src='images/login-image.png' width="700vmin" height="700vmin" alt='picture'/>
 
-        </div>
-            
-            
-            
-         
+        </div>            
     </div>
   );
 };
