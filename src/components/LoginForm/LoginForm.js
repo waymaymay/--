b0899b32d1f94748616de516dfa39ae125226a44
waymaymay <waymaymay@gmail.com>
2023-10-06 19:58:49 +0800

    新增加登入密碼驗證

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -4,11 +4,25 @@ import React, { useState } from 'react';
 import './LoginForm.css';
 import CustButton from '../Button/CustButton';
 import Leave from '../leaveButton/leave';
+import {Alert} from 'react-bootstrap';
+import { useHistory, useNavigate } from 'react-router-dom';
+import { click } from '@testing-library/user-event/dist/click';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showAlert , setShowAlert]= useState(false);
+  const navigate =useNavigate();
 
+  const handleLogin = () =>{
+    if (username === 'Vicky' && password === '12345'){
+      navigate('/FormPage');
+    } else {
+      setShowAlert(true);
+    }
+  };
+  
+  
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
@@ -16,34 +30,63 @@ const LoginForm = () => {
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
-
-  const handleLogin = (e) => {
-    // Add login logic here using 'username' and 'password'
-    console.log(e);
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
-
-  return (
+  return(
     <div>
       <div className='textgroup'>
         <label>帳號：</label>
-        <input type="text" value={username} onChange={handleUsernameChange} />
+        <input
+          type= 'text'
+          value={username}
+          onChange={handleUsernameChange}/>
       </div>
-
       <div className='passwordgroup'>
         <label>密碼：</label>
         <input
-          type="password"
+          type='password'
           value={password}
-          onChange={handlePasswordChange}
-        />
+          onChange={handlePasswordChange}/>
       </div>
-      
-      <CustButton label="登入" click={handleLogin} />
-      <Leave label="離開" />
-    </div>
-  );
+ 
+  < CustButton label='登入' click={handleLogin}/>
+  < Leave label='離開' />
+
+{showAlert && (
+  <Alert
+    variant='danger'
+    onclose={()=>setShowAlert(false)}
+    dismissible >
+    登入失敗
+  </Alert>
+  )}
+</div>
+);
 };
+
+  // const handleLogin = (e) => {
+  //   // Add login logic here using 'username' and 'password'
+  //   console.log(e);
+  //   console.log('Username:', username);
+  //   console.log('Password:', password);
+
+
+//   return (
+//     <div>
+//       <div className='textgroup'>
+//         <label>帳號：</label>
+//         <input type="text" value={username} onChange={handleUsernameChange} />
+//       </div>
+//       <div className='passwordgroup'>
+//         <label>密碼：</label>
+//         <input
+//           type="password"
+//           value={password}
+//           onChange={handlePasswordChange}
+//         />
+//       </div>
+      
+//       <CustButton label="登入" click={handleLogin} />
+//       <Leave label="離開" />
+//     </div>
+//   );
 
 export default LoginForm;
