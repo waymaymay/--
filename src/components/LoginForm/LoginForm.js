ebffdf0930a4bf7dea8d120dacf5c54070b93088
waymaymay <Rdvickywei@gmail.com>
2023-10-23 19:19:34 +0800

    update 標題

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,17 +5,22 @@ import './LoginForm.css';
 import CustButton from '../Button/CustButton';
 import Leave from '../leaveButton/leave';
 import {Alert} from 'react-bootstrap';
-import { useHistory, useNavigate } from 'react-router-dom';
-import { click } from '@testing-library/user-event/dist/click';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showAlert , setShowAlert]= useState(false);
-  const navigate =useNavigate();
+  const navigate = useNavigate();
+
+  var users = [
+    {account: "vicky", password: "12345"},
+    {account: "jay", password: "123"}
+  ];
 
   const handleLogin = () =>{
-    if (username === 'Vicky' && password === '12345'){
+    var logingUser = { account: username, password: password }
+    if (users.some(u => JSON.stringify(u) === JSON.stringify(logingUser))){
       navigate('/FormPage');
     } else {
       setShowAlert(true);
@@ -61,32 +66,5 @@ const LoginForm = () => {
 </div>
 );
 };
-
-  // const handleLogin = (e) => {
-  //   // Add login logic here using 'username' and 'password'
-  //   console.log(e);
-  //   console.log('Username:', username);
-  //   console.log('Password:', password);
-
-
-//   return (
-//     <div>
-//       <div className='textgroup'>
-//         <label>帳號：</label>
-//         <input type="text" value={username} onChange={handleUsernameChange} />
-//       </div>
-//       <div className='passwordgroup'>
-//         <label>密碼：</label>
-//         <input
-//           type="password"
-//           value={password}
-//           onChange={handlePasswordChange}
-//         />
-//       </div>
-      
-//       <CustButton label="登入" click={handleLogin} />
-//       <Leave label="離開" />
-//     </div>
-//   );
 
 export default LoginForm;
